6d3f24a1be0531321e29caa2c8604b749b6f8815
wilsonflying <wilsonfly.shs@gmail.com>
2014-07-26 16:49:47 +0800

    modify:reorganize bisearch

--- origin_c/data_structure/search/bisearch.c
+++ origin_c/data_structure/search/bisearch.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int bisearch(int *a, int n, int k)
+static int bisearch(int* data, int size, int key);
+static void print_data(int* data, int num);
+
+int main()
+{
+	int data[]={1,3,5,7,9,10,12,14,15,16,17,18,19,22,25,30,33,35};
+	int key = 0, index =0;
+
+	print_data(data,sizeof(data)/sizeof(int));
+	printf("input one num from these to find it:");
+	scanf("%d", &key);
+	//index = bisearch_no0(data, sizeof(data)/sizeof(int), key);
+	index = bisearch(data, sizeof(data)/sizeof(int), key);
+	printf("index:%d\n",index);
+}
+
+static int bisearch(int* data, int size, int key)
+{
+	if( data==NULL || size<=0 )
+		return -1;
+
+	int front = 0, rear = size-1;
+	int mid=-1;
+	while( front <= rear )/*allowed to get one same position*/
+	{
+		mid = (front+rear)/2;
+		//printf("mid:%d,front:%d,rear:%d\n",mid, front, rear);
+		if(data[mid]<key)
+			front = mid + 1;//front = mid;/*should calculate with 1,otherwise cannot get out when searching a noexist key*/
+		else if(data[mid]>key)
+			rear = mid - 1;//rear = mid;
+		else
+			return mid;
+	}
+	return -1;
+}
+
+//old version
+int bisearch_no0(int *a, int n, int k)
 {
 	int low, mid, high;
 	low = 0;
@@ -21,15 +58,16 @@ int bisearch(int *a, int n, int k)
 	return -1;
 }
 
-int main()
+static void print_data(int* data, int num)
 {
-	int a[] = {1, 3, 5, 7, 9};
-	int ret, k;
-	while(scanf("%d", &k) == 1)
-	{
-		ret = bisearch(a, sizeof(a) / sizeof(int), k);
-		printf("%d\n", ret);
-	}
+	if( data==NULL || num==0 )
+		return;
 
-	return 0;
+	int i = 0;
+	for( i=0; i < num; i++ )
+	{
+		//printf( "%d ", data[i]);
+		printf( "[%d]:%d ", i, data[i]);
+	}
+	printf("\n");
 }
