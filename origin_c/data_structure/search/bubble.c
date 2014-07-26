7f3ca03f825824630f0241479309beb9278b4590
wilsonflying <wilsonfly.shs@gmail.com>
2014-07-26 17:54:21 +0800

    modify:reorganize the bubble_sort

--- origin_c/data_structure/search/bubble.c
+++ origin_c/data_structure/search/bubble.c
@@ -1,112 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define DATA_NUM 20
 static void print_data(int* data, int num);
-
-static void bubble_sort1(int *data, int n)
-{
-	if( data==NULL || n<=1 )
-		return;
-	int i = 0;
-	int j = 0;
-	int t = 0;
-	for( i = 0; i < n-1; i++ )
-	{
-		for( j = 0; j < n-1-i; j++ )
-		{
-			if( data[j] > data[j+1] )
-			{
-				t = data[j];
-				data[j] = data[j+1];
-				data[j+1] = t;
-			}
-		}
-	}
-
-}
-
-static void bubble_sort2(int *data, int n)
-{
-	if( data==NULL || n<=1 )
-		return;
-	int i = 0;
-	int j = 0;
-	int t = 0;
-	int index = 0;
-	for( i = 0; i < n-1; i++ )
-	{
-		index = 0;
-		for( j = 0; j < n-1-i; j++ )
-		{
-			if( data[index] < data[j+1] )
-			{
-				index = j+1;
-			}
-		}
-
-		t = data[index];
-		data[index] = data[n-1-i];
-		data[n-1-i] = t;
-	}
-
-}
-
-static void bubble_sort3(int *data, int n)
-{
-	if( data==NULL || n<=1 )
-		return;
-	int i = 0;
-	int j = 0;
-	int t = 0;
-	int index = 0;
-	int flag = 0;
-	for( i = 0; i < n-1; i++ )
-	{
-		index = 0;
-		for( j = 0; j < n-1-i; j++ )
-		{
-			if( data[index] < data[j+1] )
-			{
-				index = j+1;
-			}
-			if(data[j] > data[j+1])
-			{
-				flag = 1;
-			}
-		}
-
-		//if already sorted,do nothing anymore
-		if(flag == 0)
-			return;
-
-		t = data[index];
-		data[index] = data[n-1-i];
-		data[n-1-i] = t;
-	}
-}
+static void bubble_sort0(int* data, int size);
+static void bubble_sort1(int* data, int size);
+static void bubble_sort(int* data, int size);
 
 int main()
 {
 	int i = 0;
-	int data[10];
+	int data[DATA_NUM];
 
-	srand(3);
-	for ( i = 0; i < 10; i++ )
+	srand(4);
+	for ( i = 0; i < DATA_NUM; i++ )
 	{
 		data[i] = (float)rand()/RAND_MAX*20;
 	}
 
 	print_data(data,sizeof(data)/sizeof(int));
 
-	//	bubble_sort1(data,sizeof(data)/sizeof(int));
-	//	bubble_sort2(data,sizeof(data)/sizeof(int));
-	bubble_sort3(data,sizeof(data)/sizeof(int));
+	//bubble_sort0(data,sizeof(data)/sizeof(int));
+	//bubble_sort1(data,sizeof(data)/sizeof(int));
+	bubble_sort(data,sizeof(data)/sizeof(int));
 
 	print_data(data,sizeof(data)/sizeof(int));
 
 	return 0;
 }
 
+static void bubble_sort(int* data, int size)
+{
+	if( data==NULL || size<=1 )
+		return;
+
+	int i=0, j=0;
+	int tmp = 0;
+	int max_index = 0;
+	int sort_flag = 0;
+	for( i=0; i<size-1; i++ )
+	{
+		max_index = 0;
+		for( j=0; j<size-1-i; j++ )
+		{
+			if(data[j+1]>data[max_index])/*border problem*/
+			{
+				max_index = j+1;
+				//sort_flag = 1;/*only meaned data[0] is the minumun in this round*/
+			}
+			if(data[j]<data[j+1])
+			{
+				sort_flag = 1;
+			}
+		}
+
+		if( sort_flag == 0 )
+			return;
+
+		tmp = data[j];/*j==size-1-n, the last position*/
+		data[j] = data[max_index];
+		data[max_index] = tmp;
+	}
+	return;
+}
+
+static void bubble_sort1(int* data, int size)
+{
+	if( data==NULL || size<=1 )
+		return;
+
+	int i=0, j=0;
+	int tmp = 0;
+	int max_index = 0;
+	for( i=0; i<size-1; i++ )
+	{
+		max_index = 0;
+		for( j=0; j<size-1-i; j++ )
+		{
+			if(data[j+1]>data[max_index])/*border problem*/
+			{
+				max_index = j+1;
+			}
+		}
+		tmp = data[j];/*j==size-1-n, the last position*/
+		data[j] = data[max_index];
+		data[max_index] = tmp;
+	}
+	return;
+}
+
+static void bubble_sort0(int* data, int size)
+{
+	if( data==NULL || size<=1 )
+		return;
+
+	int i=0, j=0;
+	int tmp = 0;
+	for( i=0; i<size-1; i++ )
+	{
+		for( j=0; j<size-i-1; j++ )
+		{
+			if(data[j]>data[j+1])
+			{
+				tmp = data[j];
+				data[j] = data[j+1];
+				data[j+1] = tmp;
+			}
+		}
+	}
+	return;
+}
 static void print_data(int* data, int num)
 {
 	if( data==NULL || num==0 )
