fae7be25a2c961fd3d8376b8775964783e899f50
sunhuasheng <sunhuasheng@sunniwell.net>
2015-02-08 18:03:27 +0800

    modify:update java workspace

--- java/workspace/testButtons/gen/com/wilsonflying/testbuttons/BuildConfig.java
+++ java/workspace/testButtons/gen/com/wilsonflying/testbuttons/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.wilsonflying.testbuttons;
 
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+	public final static boolean DEBUG = true;
 }
--- java/workspace/testButtons/gen/com/wilsonflying/testbuttons/R.java
+++ java/workspace/testButtons/gen/com/wilsonflying/testbuttons/R.java
@@ -11,10 +11,29 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_001=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int login=0x7f020002;
+        public static final int my_icon=0x7f020003;
+    }
+    public static final class id {
+        public static final int button_checkbox=0x7f06000a;
+        public static final int button_radiobutton=0x7f060004;
+        public static final int checkbox_music=0x7f060008;
+        public static final int checkbox_pinting=0x7f060009;
+        public static final int checkbox_sport=0x7f060007;
+        public static final int imagebutton=0x7f060005;
+        public static final int linearlayout_no1=0x7f060000;
+        public static final int linearlayout_no2=0x7f060006;
+        public static final int radiobutton1=0x7f060002;
+        public static final int radiobutton2=0x7f060003;
+        public static final int radiogroup=0x7f060001;
+    }
+    public static final class layout {
+        public static final int my_layout=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,10 +57,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
