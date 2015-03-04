5ac2951a9fa09f6e46760452f008af571293556b
sunhuasheng <sunhuasheng@sunniwell.net>
2015-03-04 23:29:54 +0800

    update workspace

---
+++ java/workspace/testIntent/src/com/wilsonflying/testintent/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.wilsonflying.testintent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle param){
+		super.onCreate(param);
+		setContentView(R.layout.layout_second);
+	}
+}
