f63422e0f6431bb68abad9d070b914ea3cec15eb
wilsonfly <wilsonfly.shs@gmail.com>
2015-04-08 00:34:37 +0800

    update workspace

--- java/workspace/testHandler/gen/com/wilsonflying/testhandler/R.java
+++ java/workspace/testHandler/gen/com/wilsonflying/testhandler/R.java
@@ -12,9 +12,19 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int img001=0x7f020001;
+        public static final int img002=0x7f020002;
+        public static final int img003=0x7f020003;
+    }
+    public static final class id {
+        public static final int image=0x7f060001;
+        public static final int text=0x7f060000;
+    }
+    public static final class layout {
+        public static final int layout_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
     public static final class style {
         /** 
@@ -38,10 +48,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
