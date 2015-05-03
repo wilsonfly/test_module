f7b2f05cef623fa19db4fc8d467be566da994a2f
wilsonfly <wilsonfly.shs@gmail.com>
2015-05-04 00:40:14 +0800

    update workspace

--- java/workspace/testImageSwitcher2/gen/com/wilsonflying/testimageswitcher2/BuildConfig.java
+++ java/workspace/testImageSwitcher2/gen/com/wilsonflying/testimageswitcher2/BuildConfig.java
@@ -2,5 +2,5 @@
 package com.wilsonflying.testimageswitcher2;
 
 public final class BuildConfig {
-	public final static boolean DEBUG = true;
+    public final static boolean DEBUG = true;
 }
---
+++ java/workspace/testMediaPlayer5/gen/com/wilsonflying/playMusic/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wilsonflying.playMusic;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
