bfcfbf0c00a16de3daa5093c38cffc2563c69654
wilsonfly <wilsonfly.shs@gmail.com>
2015-05-10 12:39:33 +0800

    add test of link *.a

--- origin_c/section/test.c
+++ origin_c/section/test.c
@@ -9,14 +9,14 @@ static int static_global2_d=1;
 
 extern void extern_foo();
 
-void foo()
+void fun_foo_T()
 {
 	static int static_internal1_b;
 	static int static_internal2_d=1;
 	time(0);
 }
 
-static void bar()
+static void static_fun_bar_t()
 {
 	printf("hello\n");
 }
@@ -26,8 +26,8 @@ int main()
 	int local1_b;
 	int local2_d=0;
 
-	foo();
-	bar();
+	fun_foo_T();
+	static_fun_bar_t();
 	extern_foo();
 
 	return 0;
