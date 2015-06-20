59018b5b22487a2e251b33a39d7df219eded9c1a
wilsonfly <wilsonfly.shs@gmail.com>
2015-06-20 23:11:43 +0800

    update worksapce

---
+++ java/workspace/testFragmentBackStack/gen/com/wilsonflying/testfragmentbackstack/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wilsonflying.testfragmentbackstack;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
