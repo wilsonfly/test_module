7d366d5d0415b06b09812d98fc2035f51eeb53e3
wilsonfly <wilsonfly.shs@gmail.com>
2015-07-18 01:23:17 +0800

    update workspace

---
+++ java/workspace/testPullToRefreshFooter/gen/com/wilsonflying/testpulltorefreshfooter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.wilsonflying.testpulltorefreshfooter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
