27e4c19b82c1d456c7ad6ca1aadd6f264020b3e5
wilsonfly <wilsonfly.shs@gmail.com>
2018-10-30 23:49:58 +0800

    add some go code
    Signed-off-by: wilsonfly <wilsonfly.shs@gmail.com>

---
+++ go/peanutGoProject/src/flagsDemo/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	//sample1()
+	sample2()
+}
+
+func sample1() {
+	var namePtr *string
+	var agePtr *int
+	namePtr = flag.String("name", "peanut", "input one name")
+	agePtr = flag.Int("age", -1, "input age")
+	flag.Parse()
+	fmt.Println(*namePtr, *agePtr)
+}
+
+func sample2() {
+	var name string
+	var age int
+	flag.StringVar(&name, "name", "peanut", "input one name")
+	flag.IntVar(&age, "age", -1, "input age")
+	flag.Parse()
+	fmt.Println(name, age)
+}
--- go/peanutGoProject/src/stringDemo/xmlMarshal.go
+++ go/peanutGoProject/src/stringDemo/xmlMarshal.go
@@ -6,18 +6,22 @@ import (
 )
 
 type person struct {
-	Name string
-	Age  int
+	Name string `xml:"name"`//`xml:"name,attr"`
+	Age  int `xml:"年龄,attr"`
 }
 
 func main() {
 	p := person{Name: "peanut", Age: 18}
-	if b, err := xml.MarshalIndent(p, "", " "); err!=nil{
+	if b, err := xml.MarshalIndent(p, "", " "); err != nil {
 		fmt.Println(err)
 		return
-	}else {
+	} else {
 		//fmt.Println(b)
 		fmt.Println(string(b))
+
+		p2 := new(person)
+		xml.Unmarshal(b, p2)
+		fmt.Println(p2)
 	}
 
 	//fmt.Println(string(b)) //b的作用域到不了这里
