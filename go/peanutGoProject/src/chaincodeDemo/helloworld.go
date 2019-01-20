4826fa80164fcd2d55666f7c5201609dce56462e
wilsonfly <wilsonfly.shs@gmail.com>
2019-01-20 16:00:07 +0800

    add some go code
    Signed-off-by: wilsonfly <wilsonfly.shs@gmail.com>

--- go/peanutGoProject/src/chaincodeDemo/helloworld.go
+++ go/peanutGoProject/src/chaincodeDemo/helloworld.go
@@ -24,12 +24,27 @@ func (h *HelloWorld) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 	fmt.Println("====chaincodeDemo in Invoke")
 
 	fn, args := stub.GetFunctionAndParameters()
+	fmt.Println("GetFunctionAndParameters:", fn, args)
+	stringArgs := stub.GetStringArgs()
+	fmt.Println("GetStringArgs:", stringArgs)
 
 	switch fn {
 	case "set":
 		return h.set(stub, args)
 	case "get":
 		return h.get(stub, args)
+	case "delete":
+		return h.delete(stub, args)
+	case "getByRange":
+		return h.getByRange(stub, args)
+	case "getHistory":
+		return h.getHistory(stub, args)
+	case "compositeKey":
+		return h.compositeKey(stub, args)
+	case "getQueryResult":
+		return h.getQueryResult(stub, args)
+		//case "InvokeChaincode":
+		//TODO: call another chaincode
 	}
 	return shim.Error("Invoke failed")
 }
@@ -52,6 +67,101 @@ func (h *HelloWorld) set(stub shim.ChaincodeStubInterface, args []string) peer.R
 	if err != nil {
 		return shim.Error(err.Error())
 	}
+	return shim.Success(nil)
+}
+
+func (h *HelloWorld) delete(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Println("====chaincodeDemo in delete")
+
+	err := stub.DelState(args[0])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(nil)
+}
+
+func (h *HelloWorld) getByRange(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Println("====chaincodeDemo in getByRange")
+
+	iterator, err := stub.GetStateByRange(args[0], args[1])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer iterator.Close()
+
+	result := []byte{}
+	for iterator.HasNext() {
+		kv, _ := iterator.Next()
+		result = append(result, kv.Value...)
+		fmt.Println("====chaincodeDemo in getByRange, result:", string(kv.Key), string(kv.Value))
+	}
+	return shim.Success(result)
+}
+
+func (h *HelloWorld) getHistory(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Println("====chaincodeDemo in getHistory")
+
+	iterator, err := stub.GetHistoryForKey(args[0])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer iterator.Close()
+
+	result := []byte{}
+	for iterator.HasNext() {
+		kv, _ := iterator.Next()
+		result = append(result, kv.Value...)
+		fmt.Println("====chaincodeDemo in getHistory, result:", string(kv.TxId), string(kv.Value))
+		fmt.Println("====chaincodeDemo in getHistory, result:", string(kv.String()))
+	}
+	return shim.Success(result)
+}
+
+func (h *HelloWorld) compositeKey(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Println("====chaincodeDemo in compositeKey")
+
+	indexName := "sexName"
+	indexKey, _ := stub.CreateCompositeKey(indexName, []string{"boy", "xiaoming"})
+	fmt.Println(indexKey)
+	stub.PutState(indexKey, []byte("hello"))
+
+	indexKey, _ = stub.CreateCompositeKey(indexName, []string{"boy", "xiaohua"})
+	fmt.Println(indexKey)
+	stub.PutState(indexKey, []byte("hello"))
+
+	indexKey, _ = stub.CreateCompositeKey(indexName, []string{"girl", "xiaoli"})
+	fmt.Println(indexKey)
+	stub.PutState(indexKey, []byte("hello"))
+
+	iterator, _ := stub.GetStateByPartialCompositeKey(indexName, []string{"boy"})
+	defer iterator.Close()
+
+	for iterator.HasNext() {
+		kv, _ := iterator.Next()
+		name, parts, _ := stub.SplitCompositeKey(kv.Key)
+		fmt.Println("indexName:", name)
+		fmt.Println("parts[0]:", parts[0])
+		fmt.Println("parts[1]:", parts[1])
+	}
+
+	return shim.Success(nil)
+}
+
+func (h *HelloWorld)getQueryResult(stub shim.ChaincodeStubInterface, args []string) peer.Response  {
+	fmt.Println("====chaincodeDemo in getQueryResult")
+
+	iterator, err := stub.GetQueryResult("{\"selector\": {\"sexName\": \"boy\"}}")
+	if err != nil {
+		return shim.Error("getqueryresult failed")
+	}
+
+	defer iterator.Close()
+
+	for iterator.HasNext() {
+		kv, _ := iterator.Next()
+		fmt.Println(kv)
+	}
+
 	return shim.Success(nil)
 }
 
--- go/peanutGoProject/src/chaincodeDemo/readme.txt
+++ go/peanutGoProject/src/chaincodeDemo/readme.txt
@@ -12,3 +12,18 @@ peer chaincode install -p chaincodedev/chaincode/chaincodeDemo -n peanut -v 0
 peer chaincode instantiate -n peanut -v 0 -c '{"Args":["hw","hello"]}' -C myc
 peer chaincode invoke -n peanut -c '{"Args":["set","hw","helloooooo"]}' -C myc
 peer chaincode invoke -n peanut -c '{"Args":["get","hw"]}' -C myc
+
+peer chaincode invoke -n peanut -c '{"Args":["delete","hw"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["get","hw"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["set","hw","helloooooo"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["get","hw"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["set","hw1","helloooooo"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["set","hw2","helloooooo"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["set","hw3","helloooooo"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["set","hw5","helloooooo"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["getByRange","hw2","hw5"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["getByRange","hw5","hw2"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["getHistory","hw"]}' -C myc
+peer chaincode invoke -n peanut -c '{"Args":["compositeKey","hw"]}' -C myc
+##fail to call getQueryResult
+peer chaincode invoke -n peanut -c '{"Args":["getQueryResult"]}' -C myc
